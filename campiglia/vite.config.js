43343a68ef1b49a17d8b910f604ccb0112aee5dc
Ricardo Gabriel Rouco <r.g.rouco1@gmail.com>
2023-10-04 01:01:56 -0300

    Update vite.config.js

--- campiglia/vite.config.js
+++ campiglia/vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'firebase/app',
-      'firebase/firestore', // Add the required Firebase modules here
-    ],
-  },
-  build: {
-    rollupOptions: {
-      external: ['firebase/firestore', 'firebase/app'], // Add the Firebase module to externalize
-    },
-  },
+  plugins: [react()]
+  // optimizeDeps: {
+  //   include: [
+  //     'firebase/app',
+  //     'firebase/firestore', // Add the required Firebase modules here
+  //   ],
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['firebase/firestore', 'firebase/app'], // Add the Firebase module to externalize
+  //   },
+  // },
 })
